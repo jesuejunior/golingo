b86d88a10839ba642f992dcaf3e69de3a244f984
JesueJunior <jesuesousa@gmail.com>
2015-11-21 21:32:01 -0200

    Add initial url to question

--- golingo/urls.py
+++ golingo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from quiz.views import QuestionTemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^question/$', QuestionTemplateView.as_view(), name='question'),
 ]
