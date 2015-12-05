89730d3eee384f89ea222257638f05721c682d3f
Flavio Augusto <fanlfilho@gmail.com>
2015-12-05 17:35:43 -0200

    Adding signin step

--- features/steps/signin.py
+++ features/steps/signin.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 from behave import given, then, when
+from model_mommy import mommy
+from django.contrib.auth.models import User
 
 
-@given(u'Nicolas, an user')
+@given(u'Barbudao, an user')
 def step_impl(context):
-    print('pass')
+    mommy.make(User, username='barbudao', email='barbudao@barbudao.com',
+               password='1234')
+
+    assert User.objects.count() == 1
 
 
-@when(u'Nicolas logs in')
+@when(u'Barbudao logs in')
 def step_impl(context):
-    print('pass')
+    br = context.browser
+    response = br.get(context.browser_url('/accounts/login/'))
+    assert response.status_code
+
+    # br.select_form(nr=0)
+    # br.form['username'] = 'barbudao'
+    # br.form['password'] = '1234'
+    # br.submit()
 
 
 @then(u'he sees the foo blah')
 def step_impl(context):
-    print('pass')
+    assert False
