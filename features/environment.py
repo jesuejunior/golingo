c518013f02b3fbebaf46476912d230fdcbeea8f1
Victor Hugo Novais <victor.novais@sieve.com.br>
2015-12-05 19:55:50 -0200

    Trying to run firts tests

--- features/environment.py
+++ features/environment.py
@@ -7,12 +7,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'golingo.settings'
 def before_all(context):
     import wsgi_intercept
     from django.core.handlers.wsgi import WSGIHandler
-    from urllib.parse import urljoin
     host = context.host = 'localhost'
     port = context.port = 8000
-    # NOTE: Nothing is actually listening on this port. wsgi_intercept
-    # monkeypatches the networking internals to use a fake socket when
-    # connecting to this port.
     wsgi_intercept.add_wsgi_intercept(host, port, WSGIHandler)
 
     def browser_url(url):
@@ -20,12 +16,12 @@ def before_all(context):
 
         e.g context.browser_url('/'), context.browser_url(reverse('my_view'))
         """
-        return urljoin('http://%s:%d/' % (host, port), url)
+        return 'http://' + host + ':' + str(port) + url
 
     context.browser_url = browser_url
 
-    ### BeautifulSoup is handy to have nearby. (Substitute lxml or html5lib as you see fit)
     from bs4 import BeautifulSoup
+
     def parse_soup():
         """Use BeautifulSoup to parse the current response and return the DOM tree.
         """
--- features/questions.feature
+++ features/questions.feature
@@ -1,9 +1,9 @@
-Feature: Questions
-
-  Scenario: List questions in lesson
-
-  Scenario: List questions in lesson with image
-
-  Scenario: List questions in lesson with audio
-
-  Scenario: List answers in question
+#Feature: Questions
+#
+#  Scenario: List questions in lesson
+#
+#  Scenario: List questions in lesson with image
+#
+#  Scenario: List questions in lesson with audio
+#
+#  Scenario: List answers in question
