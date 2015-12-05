83dc6599a4b2dbbb88f9b83ffde3cc7ad8a920b6
JesueJunior <jesuesousa@gmail.com>
2015-12-05 12:53:47 -0200

    add home route

--- quiz/views.py
+++ quiz/views.py
@@ -6,6 +6,23 @@ from django.views.generic import View
 from quiz.models import Question
 
 
+class HomeTemplateView(View):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+        cxt = self.get_context_data()
+        return TemplateResponse(
+            request=self.request,
+            template=self.template_name,
+            context=cxt,
+            **kwargs
+        )
+
+    def get_context_data(self, **kwargs):
+        cxt = {}
+        return cxt
+
+
 class QuestionTemplateView(View):
     template_name = 'question.html'
 
