cdb8a6d2378ed27a94cb0b19ece93ae9e7c25496
Nicolas Teodosio <nicolasteodosio@yahoo.com.br>
2015-12-09 14:25:01 -0200

    Fixing bd name

--- golingo/settings_test.py
+++ golingo/settings_test.py
@@ -3,7 +3,7 @@ from golingo.settings import *
 
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': "::memory::",
+    'NAME': ":memory:",
 }
 
 # LOGGING_CONFIG = True
