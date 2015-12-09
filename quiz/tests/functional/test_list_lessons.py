107f25ca19c3fdfd2bc37d12fa6671fc4307b9fd
Nicolas Teodosio <nicolasteodosio@yahoo.com.br>
2015-12-09 13:07:49 -0200

    Writing tests for lists lessons

--- quiz/tests/functional/test_list_lessons.py
+++ quiz/tests/functional/test_list_lessons.py
@@ -25,13 +25,17 @@ def test_list_lessons_available():
 
 
 @given('Jack has completed "Lesson 1"')
-def jack_has_completed_lesson_1():
+def jack_has_completed_lesson_1(browser):
     """Jack has completed "Lesson 1"."""
 
 
 @given('Jack, an logged user in our system')
-def jack_an_logged_user_in_our_system():
+def jack_an_logged_user_in_our_system(browser):
     """Jack, an logged user in our system."""
+    browser.visit('http://localhost:8000')
+    browser.fill('username', 'admin')
+    browser.fill('password', 'admin')
+    browser.find_by_id('submit').first.click()
 
 
 @given('Lessons available are:')
@@ -55,16 +59,21 @@ def _unity_2__lesson_2__medium____():
 
 
 @when('Jack is in the home page')
-def jack_is_in_the_home_page():
+def jack_is_in_the_home_page(browser):
     """Jack is in the home page."""
+    assert 'http://localhost:8000/' in browser.url
 
 
 @then('Jack gets a list of available lessons')
-def jack_gets_a_list_of_available_lessons():
+def jack_gets_a_list_of_available_lessons(browser):
     """Jack gets a list of available lessons."""
+    browser.is_element_present_by_text('Level 1 - Present Continuous (I am doing)')
+    browser.is_element_present_by_text('Level 2 - Past Continuos')
+    browser.is_element_present_by_text('Level 3 - Presente Perfect')
 
 
 @then('Jack sees that "Lesson 1" is completed')
-def jack_sees_that_lesson_1_is_completed():
+def jack_sees_that_lesson_1_is_completed(browser):
     """Jack sees that "Lesson 1" is completed."""
+    browser.is_element_present_by_css('.fui-check-inverted')
 
